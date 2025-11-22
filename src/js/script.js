ef3820c15725859fc09c7e9ce9a68e68adedb0a4
Vtr.bwr <vrodriguespereira11@gmail.com>
2025-11-22 09:52:11 -0300

    Update script.js

--- src/js/script.js
+++ src/js/script.js
@@ -203,3 +203,14 @@ document.querySelectorAll('.faq-question').forEach(button => {
       faqItem.classList.toggle('active');
     });
   });
+document.querySelectorAll(".faq-question").forEach(btn => {
+    btn.addEventListener("click", () => {
+        const item = btn.parentElement;
+
+        document.querySelectorAll(".faq-item").forEach(i => {
+            if (i !== item) i.classList.remove("active");
+        });
+
+        item.classList.toggle("active");
+    });
+});
